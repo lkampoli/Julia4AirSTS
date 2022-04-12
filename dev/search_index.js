2f8cfb36eb78272287d19ca045f4cf866297a2c9
Documenter.jl <documenter@juliadocs.github.io>
2022-04-12 04:53:30 +0000

    build based on 8276515

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Julia4AirSTS","category":"page"},{"location":"#Julia4AirSTS","page":"Home","title":"Julia4AirSTS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Julia4AirSTS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Julia4AirSTS]","category":"page"}]
+}
